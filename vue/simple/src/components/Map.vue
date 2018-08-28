40d1beda953fab118b720d7d2f19f5868a49d053
Jesús Arroyo Torrens <jarroyo@cartodb.com>
2018-08-28 13:51:06 +0200

    Extract Map methods

--- vue/simple/src/components/Map.vue
+++ vue/simple/src/components/Map.vue
@@ -3,7 +3,6 @@
 </template>
 
 <script>
-
 // TODO: change to `import carto from '@carto/carto-vl'` for next releases
 import * as carto from '@carto/carto-vl'
 import mapboxgl from '@carto/mapbox-gl'
@@ -22,33 +21,40 @@ export default {
     }
   },
   mounted: function () {
-    const basemaps = {
-      'voyager': 'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json',
-      'positron': 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json',
-      'dark-matter': 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json'
-    }
-
-    const datasets = {
-      'points': 'ne_10m_populated_places_simple',
-      'lines': 'ne_50m_rivers_lake_centerlines',
-      'polygons': 'world_borders'
-    }
-
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: basemaps[this.basemap],
-      center: [0, 30],
-      zoom: 2
-    })
-
-    const source = new carto.source.Dataset(datasets[this.geom], {
-      user: 'cartovl',
-      apiKey: 'default_public'
-    })
-    const viz = new carto.Viz()
-    const layer = new carto.Layer('layer', source, viz)
+    const map = this.createMap();
+    const layer = this.createLayer();
 
     layer.addTo(map, 'watername_ocean')
+  },
+  methods: {
+    createMap: function () {
+      const basemaps = {
+        'voyager': 'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json',
+        'positron': 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json',
+        'dark-matter': 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json'
+      }
+
+      return new mapboxgl.Map({
+        container: 'map',
+        style: basemaps[this.basemap],
+        center: [0, 30],
+        zoom: 2
+      })
+    },
+    createLayer: function () {
+      const datasets = {
+        'points': 'ne_10m_populated_places_simple',
+        'lines': 'ne_50m_rivers_lake_centerlines',
+        'polygons': 'world_borders'
+      }
+
+      const source = new carto.source.Dataset(datasets[this.geom], {
+        user: 'cartovl',
+        apiKey: 'default_public'
+      })
+      const viz = new carto.Viz()
+      return new carto.Layer('layer', source, viz)
+    }
   }
 }
 </script>
